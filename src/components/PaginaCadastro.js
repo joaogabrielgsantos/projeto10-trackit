2bbf426eddbb687311f1759bed105d411822be19
Joao Gabriel Gangorra dos Santos <jggsantos.25@gmail.com>
2022-05-30 19:51:24 -0300

    Cadastro enviando para API

--- src/components/PaginaCadastro.js
+++ src/components/PaginaCadastro.js
@@ -3,25 +3,40 @@ import Container from "./Container";
 import logo from "../assets/images/logo.png"
 import { useState } from "react";
 import Inputs from "./Inputs";
+import axios from 'axios';
 
 
-function PaginaCadastro(){
+function PaginaCadastro() {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
     const [nome, setNome] = useState('')
     const [foto, setFoto] = useState('')
+    const [usuario, setUsuario] = useState('')
+
+
+    function Cadastrar(e) {
+        e.preventDefault();
+        const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up", {email: email, name: nome, image: foto, password: senha});
+
+        promise.then(response => {
+           setUsuario (response.data);           
+        })
+    
+
+    }
+    console.log(usuario)
 
 
 
-    return(
+    return (
         <Container>
             <img src={logo} alt="Logotipo Principal" />
-            <form>
+            <form onSubmit={Cadastrar}>
                 <Inputs>
-                    <input type="email" placeholder="email" value={email}/>
-                    <input type="password" placeholder="senha" value={senha}/>
-                    <input type="text" placeholder="nome" value={nome}/>
-                    <input type="text" placeholder="foto" value={foto}/>
+                    <input type="email" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type="password" placeholder="senha" value={senha} onChange={e => setSenha(e.target.value)} />
+                    <input type="text" placeholder="nome" value={nome} onChange={e => setNome(e.target.value)} />
+                    <input type="text" placeholder="foto" value={foto} onChange={e => setFoto(e.target.value)} />
                     <button type="submit">Cadastrar</button>
                 </Inputs>
             </form>
--- src/components/PaginaLogin.js
+++ src/components/PaginaLogin.js
@@ -1,21 +1,19 @@
 import { Link } from "react-router-dom";
 import Container from "./Container";
 import logo from "../assets/images/logo.png"
-import { useState } from "react";
 import Inputs from "./Inputs";
 
 
 function PaginaLogin(){
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
+    
     
     return(
         <Container>
             <img src={logo} alt="Logotipo Principal" />
             <form>
                 <Inputs>
-                    <input type="email" placeholder="email" value={email}/>
-                    <input type="password" placeholder="senha" value={senha}/>
+                    <input type="email" placeholder="email" />
+                    <input type="password" placeholder="senha"/>
                     <button type="submit">Entrar</button>
                 </Inputs>
             </form>
